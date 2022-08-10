29c705c755593d25ad03cfbada1bc3e1f0fdfbed
kalixevori471 <108694568+kalixevori471@users.noreply.github.com>
2022-08-11 00:36:35 +0800

    Add files via upload

---
+++ shit-ip-lookup.py
@@ -0,0 +1,21 @@
+import pygeoip # pip install pygeoip
+
+DB = pygeoip.GeoIP('GeoLiteCity.dat')
+
+def IP_INFO(IP):
+    Data = DB.record_by_addr(IP)
+    for Key, Value in Data.items():
+        print("{0} : {1}".format(Key, Value))
+    
+while True:
+    cmd = str(input("~ "))
+    if cmd == 'quit':
+        break
+    
+    if cmd == 'ip':
+        ip = str(input("Enter The Shit IP address: "))
+        try:
+            IP_INFO(ip)
+            
+        except Exception as Error:
+            print(Error)
